76221d3b4efb4d69be9b8cb5f5847752603f4dba
alicia <50755533+amfosh@users.noreply.github.com>
2021-07-17 09:12:31 -0400

    setting up

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
-function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+
+class App extends Component {
+  render () {
+    return (
+      <React.Fragment>
+        <Router>
+          <Switch>
+            <Route exact path="/" component={Home}/>
+            <Route exact path="/about" component={About}/>
+            <Route exact path="/contact" component={Contact}/>
+            <Route component={NoMatch}/>
+          </Switch>
+
+        </Router>
+      </React.Fragment>
+    )
+  };
 }
 
 export default App;
